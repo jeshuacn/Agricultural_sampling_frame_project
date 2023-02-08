e166220708c9582b95b49d667f8ef218cb35e948
jeshuacn <33787097+jeshuacn@users.noreply.github.com>
2023-02-08 03:16:57 -0500

    Update sampling_app.py

--- app/sampling_app.py
+++ app/sampling_app.py
@@ -482,6 +482,6 @@ st.markdown('**Systematic sampling** is the most appropiate method for this popu
 ''
 ''
 ''
-st.markdown('For a comprehensive understanding, please refer to the full analysis: [Notebook](https://colab.research.google.com/github/jeshuacn/Agricultural_sampling_frame_project/blob/main/Jeshua_SamplingFrame_Advanced_main.ipynb)  <img  alt="GitHub" width="26px" src="https://user-images.githubusercontent.com/3369400/139448065-39a229ba-4b06-434b-bc67-616e2ed80c8f.png" style="padding-right:10px;" />[Github](https://github.com/jeshuacn/Agricultural_sampling_frame_project/blob/main/README.md) ',unsafe_allow_html=True)
-st.markdown('[Notebook](https://colab.research.google.com/github/jeshuacn/Agricultural_sampling_frame_project/blob/main/Jeshua_SamplingFrame_Advanced_main.ipynb)')
+st.markdown('For a comprehensive understanding, please refer to the full analysis: [Notebook](https://colab.research.google.com/github/jeshuacn/Agricultural_sampling_frame_project/blob/main/Jeshua_SamplingFrame_Advanced_main.ipynb)' )
+st.markdown('<img  alt="GitHub" width="26px" src="https://user-images.githubusercontent.com/3369400/139448065-39a229ba-4b06-434b-bc67-616e2ed80c8f.png" style="padding-right:10px;" />[Github](https://github.com/jeshuacn/Agricultural_sampling_frame_project/blob/main/README.md) ',unsafe_allow_html=True)
 #################################################################################################

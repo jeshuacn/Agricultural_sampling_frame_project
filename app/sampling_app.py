2955c8914b9995f55fc6dfd1b698d43e90da8828
jeshuacn <33787097+jeshuacn@users.noreply.github.com>
2023-02-07 22:18:32 -0500

    Update sampling_app.py

--- app/sampling_app.py
+++ app/sampling_app.py
@@ -132,7 +132,7 @@ height = st.sidebar.slider("Plot height", 1, 25, 5)
 
 # Rerun button in sidebar
 st.sidebar.markdown("**Here you can perform a recaclulation of the sampling methods:**")
-bt = st.sidebar.button('Recalculate Samples',label = '')
+bt = st.sidebar.button('Recalculate Samples')
 if bt:
 
     st.experimental_memo.clear()

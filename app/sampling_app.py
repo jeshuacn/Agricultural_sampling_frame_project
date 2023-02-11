c8954eb1abebc5740f959c329c5682fcd27aa221
jeshuacn <33787097+jeshuacn@users.noreply.github.com>
2023-02-10 22:21:37 -0500

    added thanks

--- app/sampling_app.py
+++ app/sampling_app.py
@@ -485,3 +485,5 @@ st.markdown('After a lengthy analysis of each sampling method, we can conclude t
 st.markdown('For a comprehensive understanding, please refer to the full analysis: [Notebook](https://colab.research.google.com/github/jeshuacn/Agricultural_sampling_frame_project/blob/main/SamplingFrame.ipynb)' )
 st.markdown('<img  alt="GitHub" width="26px" src="https://user-images.githubusercontent.com/3369400/139448065-39a229ba-4b06-434b-bc67-616e2ed80c8f.png" style="padding-right:10px;" />[Github](https://github.com/jeshuacn/Agricultural_sampling_frame_project/blob/main/README.md) ',unsafe_allow_html=True)
 #################################################################################################
+
+st.markdown('Special thanks to Lonely Octopus :octopus:')

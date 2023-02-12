a937d3c1a6960da206ff3ebd47a6d713185ec0ff
jeshuacn <33787097+jeshuacn@users.noreply.github.com>
2023-02-12 15:36:44 -0500

    added notebook emoji

--- app/sampling_app.py
+++ app/sampling_app.py
@@ -496,7 +496,7 @@ st.markdown('After a lengthy analysis of each sampling method, we can conclude t
 ''
 ''
 ''
-st.markdown('For a comprehensive understanding, please refer to the full analysis: [Notebook](https://colab.research.google.com/github/jeshuacn/Agricultural_sampling_frame_project/blob/main/SamplingFrame.ipynb)' )
+st.markdown('For a comprehensive understanding, please refer to the full analysis: 📙 [Notebook](https://colab.research.google.com/github/jeshuacn/Agricultural_sampling_frame_project/blob/main/SamplingFrame.ipynb)' )
 st.markdown('<img  alt="GitHub" width="26px" src="https://user-images.githubusercontent.com/3369400/139448065-39a229ba-4b06-434b-bc67-616e2ed80c8f.png" style="padding-right:10px;" />[Github](https://github.com/jeshuacn/Agricultural_sampling_frame_project/blob/main/README.md) ',unsafe_allow_html=True)
 #################################################################################################
 
